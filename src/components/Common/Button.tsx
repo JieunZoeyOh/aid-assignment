06f23572e7b8ac30bed0de9f0ae5f4ba0f21afa4
Jieun Oh <ohjieun@kakao.com>
2024-09-26 22:22:51 +0900

    Feat: Button 컴포넌트 isDisabled 속성 추가, 비활성화 상태일 때 onClick 방지

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -2,6 +2,7 @@ type ButtonProps = {
   label: string;
   onClick: () => void;
   isWidthFull?: boolean;
+  isDisabled?: boolean;
   buttonType?: string;
   buttonSize?: string;
 };
@@ -11,15 +12,22 @@ export default function Button(props: ButtonProps) {
     label,
     onClick,
     isWidthFull = false,
+    isDisabled = false,
     buttonType = "btn-light",
     buttonSize = "btn-base",
   } = props;
 
+  const handleButtonClick = () => {
+    if (!isDisabled) {
+      onClick();
+    }
+  };
+
   return (
     <button
       type="button"
-      className={`${buttonType} ${buttonSize} ${isWidthFull && "w-full"} rounded-lg`}
-      onClick={onClick}
+      className={`${isDisabled ? "btn-gray" : buttonType} ${buttonSize} ${isWidthFull && "w-full"} rounded-lg`}
+      onClick={handleButtonClick}
     >
       {label}
     </button>
