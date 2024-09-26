0e46260bfafa95e1b69c6e8e99be14710f17af5a
Jieun Oh <ohjieun@kakao.com>
2024-09-26 11:56:45 +0900

    Setting: 라우터 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 
 import App from "./App.tsx";
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/timetable" replace />,
+  },
+  {
+    path: "/timetable",
+    element: <App />,
+  },
+]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
