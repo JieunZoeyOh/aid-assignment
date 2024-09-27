0cb6a5d64b7efc0f8e675295f5fc80e9ba40fa7d
Jieun Oh <ohjieun@kakao.com>
2024-09-27 18:35:22 +0900

    Feat: 슬롯이 5개로 모두 차있는 경우의 수업 삭제 구현
    - 슬롯이 5개로 모두 차있을 때, 마지막 수업을 삭제하면 다음 슬롯의 수업이 이동됩니다.
    - timetableReducer의 로직을 가독성 있게 리팩토링 했습니다.

--- src/state/timetableReducer.ts
+++ src/state/timetableReducer.ts
@@ -1,86 +1,128 @@
 import { TimetableAction, TimetableState } from "./stateTypes";
 
+const MAX_COURSES = 5;
+
 export default function reducer(
   state: TimetableState,
   action: TimetableAction,
 ): TimetableState {
   switch (action.type) {
     case "UPDATE_BREAK_TIME": {
+      const { breakType, timeType, time } = action.payload;
+
       return {
         ...state,
         breakTime: {
           ...state.breakTime,
-          [action.payload.breakType]: {
-            ...state.breakTime[action.payload.breakType],
-            [action.payload.timeType]: action.payload.time,
+          [breakType]: {
+            ...state.breakTime[breakType],
+            [timeType]: time,
           },
         },
       };
     }
     case "ADD_COURSE": {
-      const slotIndex = action.payload.slotIndex;
+      const { slotIndex } = action.payload;
 
       return {
         ...state,
-        timeSlots: state.timeSlots.map((slot, index) =>
-          index === slotIndex
-            ? {
-                ...slot,
-                courses: [
-                  ...slot.courses,
-                  {
-                    id: crypto.randomUUID(),
-                    startTime: { hour: "00", minute: "00" },
-                    endTime: { hour: "00", minute: "00" },
-                  },
-                ],
-              }
-            : slot,
-        ),
+        timeSlots: state.timeSlots.map((slot, index) => {
+          if (index !== slotIndex) return slot;
+
+          const newCourses = [
+            ...slot.courses,
+            {
+              id: crypto.randomUUID(),
+              startTime: { hour: "00", minute: "00" },
+              endTime: { hour: "00", minute: "00" },
+            },
+          ];
+
+          return {
+            ...slot,
+            courses: newCourses,
+          };
+        }),
       };
     }
     case "DELETE_COURSE": {
-      const slotIndex = action.payload.slotIndex;
-      const courseId = action.payload.courseId;
+      const { slotIndex, courseId } = action.payload;
+      const courseIndex = state.timeSlots[slotIndex].courses.findIndex(
+        ({ id }) => id === courseId,
+      );
 
-      return {
+      const result = {
         ...state,
-        timeSlots: state.timeSlots.map((slot, index) =>
-          index === slotIndex
-            ? {
-                ...slot,
-                courses: slot.courses.filter(
-                  (course) => course.id !== courseId,
-                ),
-              }
-            : slot,
-        ),
+        timeSlots: state.timeSlots.map((slot, index) => {
+          if (index !== slotIndex) return slot;
+
+          const newCourses = slot.courses.filter(
+            (course) => course.id !== courseId,
+          );
+
+          return { ...slot, courses: newCourses };
+        }),
       };
+
+      if (courseIndex === MAX_COURSES - 1) {
+        return shiftCourseEarlier(slotIndex, result);
+      }
+
+      return result;
     }
     case "UPDATE_COURSE_TIME": {
-      const slotIndex = action.payload.slotIndex;
-      const courseId = action.payload.courseId;
+      const { timeType, time, slotIndex, courseId } = action.payload;
 
-      return {
-        ...state,
-        timeSlots: state.timeSlots.map((slot, index) =>
-          index === slotIndex
-            ? {
-                ...slot,
-                courses: slot.courses.map((course) =>
-                  course.id === courseId
-                    ? {
-                        ...course,
-                        [action.payload.timeType]: action.payload.time,
-                      }
-                    : course,
-                ),
-              }
-            : slot,
-        ),
-      };
+      const newTimeSlots = state.timeSlots.map((slot, index) => {
+        if (index !== slotIndex) return slot;
+
+        const newCourses = slot.courses.map((course) => {
+          if (course.id !== courseId) return course;
+
+          return { ...course, [timeType]: time };
+        });
+
+        return { ...slot, courses: newCourses };
+      });
+
+      return { ...state, timeSlots: newTimeSlots };
     }
     default:
       return state;
   }
+}
+
+function shiftCourseEarlier(
+  slotIndex: number,
+  timetable: TimetableState,
+): TimetableState {
+  const { timeSlots } = timetable;
+
+  const isLastSlot = slotIndex >= timeSlots.length - 1;
+  const isFullSlotList = timeSlots[slotIndex].courses.length >= MAX_COURSES - 1;
+
+  if (isLastSlot || !isFullSlotList) return timetable;
+
+  const nextSlot = timeSlots[slotIndex + 1];
+
+  if (nextSlot.courses.length === 0) return timetable;
+
+  const [nextFirstCourse, ...nextRemainingCourses] = nextSlot.courses;
+
+  const newTimeSlots = timeSlots.map((slot, index) => {
+    if (index === slotIndex) {
+      return { ...slot, courses: [...slot.courses, nextFirstCourse] };
+    } else if (index === slotIndex + 1) {
+      return { ...slot, courses: nextRemainingCourses };
+    } else {
+      return slot;
+    }
+  });
+
+  const newTimetable = {
+    ...timetable,
+    timeSlots: newTimeSlots,
+  };
+
+  return shiftCourseEarlier(slotIndex + 1, newTimetable);
 }
